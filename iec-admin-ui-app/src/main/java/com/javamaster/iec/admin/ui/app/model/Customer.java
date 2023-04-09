0beabd6707e8cc3696fa552613f0dc877c65f8c7
poornae <poornae_9611@hotmail.com>
2023-04-09 07:20:13 +0530

    model

--- iec-admin-ui-app/src/main/java/com/javamaster/iec/admin/ui/app/model/Customer.java
+++ iec-admin-ui-app/src/main/java/com/javamaster/iec/admin/ui/app/model/Customer.java
@@ -4,6 +4,10 @@
  */
 package com.javamaster.iec.admin.ui.app.model;
 
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.List;
+
 /**
  *
  * @author poornae
@@ -15,16 +19,19 @@ public class Customer {
     private String username;
     private String password;
     private String email;
-    private String date_of_birth;
+    private Date date_of_birth;
     private String nic_no;
-    private String profile_image;
-    private String contact_no;
+    private byte[] profile_image;
+    private int contact_no;
     private String address;
-    private String created_at;
-    private String updated_at;
-    private String last_login_at;
+    private Timestamp created_at;
+    private Timestamp updated_at;
+    private Timestamp last_login_at;
+    private List<Loan> customers;
+    private List<Order> orders;
+    private List<Transaction> transactions;
 
-    public Customer(int customerID, String full_name, String username, String password, String email, String date_of_birth, String nic_no, String profile_image, String contact_no, String address, String created_at, String updated_at, String last_login_at) {
+    public Customer(int customerID, String full_name, String username, String password, String email, Date date_of_birth, String nic_no, byte[] profile_image, int contact_no, String address, Timestamp created_at, Timestamp updated_at, Timestamp last_login_at, List<Loan> customers, List<Order> orders, List<Transaction> transactions) {
         this.customerID = customerID;
         this.full_name = full_name;
         this.username = username;
@@ -38,9 +45,12 @@ public class Customer {
         this.created_at = created_at;
         this.updated_at = updated_at;
         this.last_login_at = last_login_at;
+        this.customers = customers;
+        this.orders = orders;
+        this.transactions = transactions;
     }
 
-    public Customer(String full_name, String username, String password, String email, String date_of_birth, String nic_no, String profile_image, String contact_no, String address, String created_at, String updated_at, String last_login_at) {
+    public Customer(String full_name, String username, String password, String email, Date date_of_birth, String nic_no, byte[] profile_image, int contact_no, String address, Timestamp created_at, Timestamp updated_at, Timestamp last_login_at, List<Loan> customers, List<Order> orders, List<Transaction> transactions) {
         this.full_name = full_name;
         this.username = username;
         this.password = password;
@@ -53,109 +63,137 @@ public class Customer {
         this.created_at = created_at;
         this.updated_at = updated_at;
         this.last_login_at = last_login_at;
+        this.customers = customers;
+        this.orders = orders;
+        this.transactions = transactions;
     }
 
     public int getCustomerID() {
         return customerID;
     }
 
-    public void setCustomerID(int customerID) {
-        this.customerID = customerID;
-    }
-
     public String getFull_name() {
         return full_name;
-    }
-
-    public void setFull_name(String full_name) {
-        this.full_name = full_name;
     }
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getDate_of_birth() {
+    public Date getDate_of_birth() {
         return date_of_birth;
-    }
-
-    public void setDate_of_birth(String date_of_birth) {
-        this.date_of_birth = date_of_birth;
     }
 
     public String getNic_no() {
         return nic_no;
     }
 
-    public void setNic_no(String nic_no) {
-        this.nic_no = nic_no;
-    }
-
-    public String getProfile_image() {
+    public byte[] getProfile_image() {
         return profile_image;
     }
 
-    public void setProfile_image(String profile_image) {
-        this.profile_image = profile_image;
-    }
-
-    public String getContact_no() {
+    public int getContact_no() {
         return contact_no;
-    }
-
-    public void setContact_no(String contact_no) {
-        this.contact_no = contact_no;
     }
 
     public String getAddress() {
         return address;
     }
 
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public Timestamp getLast_login_at() {
+        return last_login_at;
+    }
+
+    public List<Loan> getCustomers() {
+        return customers;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDate_of_birth(Date date_of_birth) {
+        this.date_of_birth = date_of_birth;
+    }
+
+    public void setNic_no(String nic_no) {
+        this.nic_no = nic_no;
+    }
+
+    public void setProfile_image(byte[] profile_image) {
+        this.profile_image = profile_image;
+    }
+
+    public void setContact_no(int contact_no) {
+        this.contact_no = contact_no;
+    }
+
     public void setAddress(String address) {
         this.address = address;
     }
 
-    public String getCreated_at() {
-        return created_at;
-    }
-
-    public void setCreated_at(String created_at) {
+    public void setCreated_at(Timestamp created_at) {
         this.created_at = created_at;
     }
 
-    public String getUpdated_at() {
-        return updated_at;
-    }
-
-    public void setUpdated_at(String updated_at) {
+    public void setUpdated_at(Timestamp updated_at) {
         this.updated_at = updated_at;
     }
 
-    public String getLast_login_at() {
-        return last_login_at;
-    }
-
-    public void setLast_login_at(String last_login_at) {
+    public void setLast_login_at(Timestamp last_login_at) {
         this.last_login_at = last_login_at;
     }
+
+    public void setCustomers(List<Loan> customers) {
+        this.customers = customers;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
 }
---
+++ iec-admin-ui-app/src/main/java/com/javamaster/iec/admin/ui/app/model/Order.java
@@ -0,0 +1,110 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.javamaster.iec.admin.ui.app.model;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.List;
+
+/**
+ *
+ * @author poornae
+ */
+public class Order {
+
+    private int orderID;
+    private Date order_date;
+    private Float order_total;
+    private String order_status;
+    private Timestamp updated_at;
+    private int updated_by;
+    private int customer_id; //foreign key
+    private List<Transaction> transactions;
+
+    public Order(int orderID, Date order_date, Float order_total, String order_status, Timestamp updated_at, int updated_by, int customer_id, List<Transaction> transactions) {
+        this.orderID = orderID;
+        this.order_date = order_date;
+        this.order_total = order_total;
+        this.order_status = order_status;
+        this.updated_at = updated_at;
+        this.updated_by = updated_by;
+        this.customer_id = customer_id;
+        this.transactions = transactions;
+    }
+
+    public Order(Date order_date, Float order_total, String order_status, Timestamp updated_at, int updated_by, int customer_id, List<Transaction> transactions) {
+        this.order_date = order_date;
+        this.order_total = order_total;
+        this.order_status = order_status;
+        this.updated_at = updated_at;
+        this.updated_by = updated_by;
+        this.customer_id = customer_id;
+        this.transactions = transactions;
+    }
+
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public Date getOrder_date() {
+        return order_date;
+    }
+
+    public Float getOrder_total() {
+        return order_total;
+    }
+
+    public String getOrder_status() {
+        return order_status;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public int getUpdated_by() {
+        return updated_by;
+    }
+
+    public int getCustomer_id() {
+        return customer_id;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
+    public void setOrder_date(Date order_date) {
+        this.order_date = order_date;
+    }
+
+    public void setOrder_total(Float order_total) {
+        this.order_total = order_total;
+    }
+
+    public void setOrder_status(String order_status) {
+        this.order_status = order_status;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public void setUpdated_by(int updated_by) {
+        this.updated_by = updated_by;
+    }
+
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+}
